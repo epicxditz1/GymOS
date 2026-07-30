ef197e1a15e80243ff65b8d243b608fccec5bd6a
Mayank <leader46974@gmail.com>
2026-07-30 15:34:29 +0530

    Refactor: Start MembersPage extraction

--- client/src/pages/MembersPage.jsx
+++ client/src/pages/MembersPage.jsx
@@ -1,7 +1,43 @@
-function MembersPage() {
+function MembersPage({
+  members,
+  search,
+  setSearch,
+  setPage,
+}) {
   return (
-    <div>
-      <h1>Members Page</h1>
+    <div className="container">
+      <h1>📋 All Members</h1>
+
+      <input
+  type="text"
+  placeholder="🔍 Search Member"
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+/>
+
+<br />
+<br />
+
+      {members.length === 0 ? (
+        <p>No Members Found</p>
+      ) : (
+        members
+  .filter((member) =>
+    member.name?.toLowerCase().includes(search.toLowerCase()) ||
+    member.phone?.includes(search)
+  )
+  .map((member) => (
+          <div key={member._id}>
+            <h3>{member.name}</h3>
+            <hr />
+          </div>
+        ))
+      )}
+
+      <button onClick={() => setPage("home")}>
+  Back
+</button>
+
     </div>
   );
 }
